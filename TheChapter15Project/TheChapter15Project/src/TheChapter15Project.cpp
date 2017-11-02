eb910de30cd308c080945e95c6c85e15cb7e0e2e
SubtleG <4bryceh@gmail.com>
2017-11-02 11:55:47 -0700

    Blank slate

---
+++ TheChapter15Project/TheChapter15Project/src/TheChapter15Project.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : TheChapter15Project.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Program ending, Have a nice day!" << endl; // prints Program ending, Have a nice day!
+	return 0;
+}
+
